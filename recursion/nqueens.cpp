dba5c0d332942fbd1c89b4e030e226f63e5878ef
Atul John <atul27790@gmail.com>
2020-03-16 08:42:12 -0700

    Create nqueens.cpp
    all time favorite n queens recursion+backtracking problem.

---
+++ recursion/nqueens.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
+#include <memory>
+#include <vector>
+
+using std::make_unique;
+using std::vector;
+
+void SolveNQueens(int, int, vector<int>&, vector<vector<int>>&);
+bool IsValid(const vector<int>);
+
+vector<vector<int>> NQueens(int n) {
+  vector<vector<int>> result;
+  vector<int> col_placement;
+  SolveNQueens(n, 0, col_placement, result);
+  return result;
+}
+
+void SolveNQueens(int n, int row, vector<int>& col_placement,
+                  vector<vector<int>>& result) {
+  if (row == n) {
+    // All queens are legally placed.
+    result.emplace_back(col_placement);
+  } else {
+    for (int col = 0; col < n; ++col) {
+      col_placement.emplace_back(col);
+      if (IsValid(col_placement)) {
+        SolveNQueens(n, row + 1, col_placement, result);
+      }
+      col_placement.pop_back();
+    }
+  }
+}
+
+// Test if a newly placed queen will conflict any earlier queens
+// placed before.
+bool IsValid(const vector<int> col_placement) {
+  int row_id = size(col_placement) - 1;
+  for (int i = 0; i < row_id; ++i) {
+    if (int diff = abs(col_placement[i] - col_placement[row_id]);
+        diff == 0 || diff == row_id - i) {
+      // A column or diagonal constraint is violated.
+      return false;
+    }
+  }
+  return true;
+}
